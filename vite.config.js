3272fe8fd1e658f858fe37858c45d5294342f0a3
Никита Домрачев <domrachev-nikitka@mail.ru>
2025-01-17 22:50:17 +0700

    /features был убран порт для сервера в конфиге

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
 
 
   server: {
-    port: 3000, // Порт для разработки
     open: true, // Автоматически открывает браузер
-    hot: true, // Включает горячую перезагрузку
+
   },
 });
