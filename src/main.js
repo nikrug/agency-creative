4d982064ce7388ebf0e0cdf767f7c5641e21ae91
Никита Домрачев <domrachev-nikitka@mail.ru>
2025-01-22 01:28:48 +0700

    /features:была сделана верстка для главной страницы так же была сделана верстка для мобилок и пк (для планшетов пока нету) реализована темная тема и анимация шариков в header

--- src/main.js
+++ src/main.js
@@ -1,41 +1,36 @@
-import './styles/reset.scss';
 import './styles/header.scss';
+import './styles/reset.scss';
+import './styles/about.scss';
+import './styles/servises.scss';
+import './styles/portfolio.scss';
+import './styles/testimonials.scss';
+import './styles/footer.scss';
 import './styles/fonts.scss';
 import { initHeader } from './header.js'; 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const include = document.querySelectorAll("include");
-  include.forEach(async (el) => {
-      const src = el.getAttribute("src");
-      const response = await fetch(src);
-      const content = await response.text();
-      el.outerHTML = content; 
-      initHeader(); 
+document.addEventListener("DOMContentLoaded", async () => {
+    const includeElements = document.querySelectorAll("include");
+    
+    for (const el of includeElements) {
+        const src = el.getAttribute("src");
+        const response = await fetch(src);
+        const content = await response.text();
+        el.outerHTML = content; // Заменяет элемент include на загруженное содержимое 
+    }
 
-  });
-
-  const includes = document.querySelectorAll('.include');
-
-  includes.forEach(include => {
-    const src = include.getAttribute('data-src');
-    fetch(src)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Ошибка при загрузке: ' + response.status);
-            }
-            return response.text();
-        })
-        .then(html => {
-            include.innerHTML = html; // Вставляем загруженный HTML
-       
-            initHeader();
-        })
-        .catch(error => {
-            console.error('Ошибка загрузки:', error);
-        });
-  });
+    initHeader(); // Инициализация после того, как весь контент загружен и добавлен в DOM
 });
 
-// Инициализация заголовка и музыки
-initHeader(); 
+
+const welcomeScreen = document.getElementById('welcomeScreen');
+
+// Убираем приветственный экран через 5 секунд
+setTimeout(() => {
+    welcomeScreen.classList.add('fadeOut'); // Добавляем класс для анимации исчезновения
+    // Удаляем элемент после завершения анимации
+    setTimeout(() => {
+        welcomeScreen.style.display = 'none'; // Скрыть приветственный экран
+    }, 8000); // Длительность анимации исчезновения
+}, 9000); 
+
  
